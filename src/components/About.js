fb5c7c66e3718e078b41c141fdfae0b95e4ed239
+ <+>
2022-03-28 21:03:16 -0400

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Links from "./Links";
 
 function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio ? props.bio : null}</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links />
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
